cd01f1d27e8a7ae7f2ae51bb5d0c538e39f02bf0
Quang Ho <mxrshmalloww@gmail.com>
2024-06-03 15:59:53 -0400

    Add project files.

---
+++ CoffeeShopPOS/Login.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CoffeeShopPOS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UserNameTxt.Text;
+            string password = PasswordTxt.Text;
+            //Connect to mysql database later
+            if (username == "testing" && password == "testing")
+            {
+                MessageBox.Show("Login Successful");
+                this.Hide();
+                Window1 window1 = new Window1();
+                window1.Show();
+            }
+            else
+            {
+                MessageBox.Show("Login Failed");
+            }
+
+
+        }
+    }
+}
---
+++ POS_Repository/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace POS_Repository
+{
+    public class Class1
+    {
+
+    }
+}
