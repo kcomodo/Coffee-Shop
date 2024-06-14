902712d6d075c3579bdf46355d700020c604cdac
Quang Ho <mxrshmalloww@gmail.com>
2024-06-13 21:18:43 -0400

    Decided on an alternative to the problem with pages, will not use pages for adding items to the cart, will just have the buttons sort the images out in the main screen.

--- CoffeeShopPOS/CoffeePage.xaml.cs
+++ CoffeeShopPOS/CoffeePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using POS_Folders.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,18 @@ namespace CoffeeShopPOS
     /// </summary>
     public partial class CoffeePage : Page
     {
-        public CoffeePage()
+        ItemModel ItemModel;
+        public CoffeePage(ItemModel IM)
         {
             InitializeComponent();
+            this.ItemModel = IM;
         }
+        public void buttonClicked(object sender, RoutedEventArgs e)
+        {
+            MainScreen mainScreen = new MainScreen();
+            mainScreen.AddToCart("Coffee", 2.50m, 2);
+           
+        }
+
     }
 }
