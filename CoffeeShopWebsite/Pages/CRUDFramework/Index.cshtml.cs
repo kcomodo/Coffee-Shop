56df219ac0f0155e54baac13889cd3b058e73a48
Quang Ho <mxrshmalloww@gmail.com>
2024-06-14 14:13:22 -0400

    Created a new CRUD framework for the website, created the customer model, will make a new branch dedicated for the website later.

---
+++ CoffeeShopWebsite/Pages/CRUDFramework/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CoffeeShopWebsite.Data;
+using POS_Folders.Models;
+
+namespace CoffeeShopWebsite.Pages.CRUDFramework
+{
+    public class IndexModel : PageModel
+    {
+        private readonly CoffeeShopWebsite.Data.CoffeeShopWebsiteContext _context;
+
+        public IndexModel(CoffeeShopWebsite.Data.CoffeeShopWebsiteContext context)
+        {
+            _context = context;
+        }
+
+        public IList<CustomerModel> CustomerModel { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            CustomerModel = await _context.CustomerModel.ToListAsync();
+        }
+    }
+}
---
+++ POS_Repository/Models/CustomerModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_Folders.Models
+{
+    public class CustomerModel
+    {
+        public int ID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerGender { get; set; }
+        public string CustomerDOB { get; set; }
+
+    }
+}
