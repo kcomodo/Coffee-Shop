f590b5f0629cb28202d17838319bda8249d347b0
Quang Ho <mxrshmalloww@gmail.com>
2024-06-20 14:39:53 -0400

    Created a login form for the page, added some validation for the email and password, created a register button that leads to the register page. Will connect to DB later to finish login form and add cs to stylize it

--- CoffeeShopWebsiteAngular/CoffeeShopWebsiteAngular.Server/CoffeeShopWebsiteFrontEnd/src/app/components/login/login.component.ts
+++ CoffeeShopWebsiteAngular/CoffeeShopWebsiteAngular.Server/CoffeeShopWebsiteFrontEnd/src/app/components/login/login.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordFormControl = new FormControl('', [Validators.required]);
 
+  constructor(private fb: FormBuilder) {
+
+  }
 }
