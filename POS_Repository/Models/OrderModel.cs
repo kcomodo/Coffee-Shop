eaa1a0c98f0cf170c09066f574ecfc94cfc92f2f
Quang Ho <mxrshmalloww@gmail.com>
2024-07-01 11:47:11 -0400

    Worked on some of the front end design, need to adjust the background image later for them and add in more context, need to fix register page background image.

--- POS_Repository/Models/OrderModel.cs
+++ POS_Repository/Models/OrderModel.cs
@@ -9,6 +9,11 @@ namespace POS_Folders.Models
     public class OrderModel
     {
         public int orderItem_id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
         public int item_id { get; set; }
         public int quantity { get; set; }
         public double unitPrice { get; set; }
