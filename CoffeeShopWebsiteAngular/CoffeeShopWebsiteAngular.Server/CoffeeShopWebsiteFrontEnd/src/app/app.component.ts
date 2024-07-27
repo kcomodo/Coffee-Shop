e2ca942064bf3487d6cd64193ddc2eb9f7f15411
Quang Ho <mxrshmalloww@gmail.com>
2024-07-27 18:24:03 -0400

    Login now changes to logout whenever you want to, and the cookie is now deleted when logged out

--- CoffeeShopWebsiteAngular/CoffeeShopWebsiteAngular.Server/CoffeeShopWebsiteFrontEnd/src/app/app.component.ts
+++ CoffeeShopWebsiteAngular/CoffeeShopWebsiteAngular.Server/CoffeeShopWebsiteFrontEnd/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { EmailServiceService } from './services/email-service.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'CoffeeShopWebsiteFrontEnd';
+  constructor(private authService: AuthService, private emailService: EmailServiceService) { }
+  ngOnInit() {
+    this.checkLogin();
+  }
+  checkLogin() {
+    if (this.authService.getToken() != null) {
+    //  console.log("Token is not null")
+    //  console.log("Token is: ", this.authService.getToken())
+      return true;
+    }
+    else {
+     // console.log("Token is null")
+      return false;
+    }
+  }
+  checkLogout() {
+    this.authService.logout();
+    this.emailService.deleteEmail();
+  }
 }
