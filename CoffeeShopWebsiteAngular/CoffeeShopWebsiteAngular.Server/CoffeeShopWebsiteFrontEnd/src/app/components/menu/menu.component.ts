f9d25ad9b47d74a4e1c52e7dd55a957064c126a1
Quang Ho <mxrshmalloww@gmail.com>
2024-08-06 10:31:28 -0400

    Finished Menu, will finish the other pages soon.

--- CoffeeShopWebsiteAngular/CoffeeShopWebsiteAngular.Server/CoffeeShopWebsiteFrontEnd/src/app/components/menu/menu.component.ts
+++ CoffeeShopWebsiteAngular/CoffeeShopWebsiteAngular.Server/CoffeeShopWebsiteFrontEnd/src/app/components/menu/menu.component.ts
@@ -13,17 +13,17 @@ export class MenuComponent {
     { name: 'cappuccino', description: 'Smooth and bold', price: 3, image: '../assets/images/cappuccino.png' },
   ];
   foods = [
-    { name: 'Egg Sandwich', description: 'Chicken, Egg, and Mapple Butter', price: 2.5, image: 'espresso.jpg' },
-    { name: 'Sausage & Egg Wrap', description: 'Sausage, Chedder, and Egg', price: 3.0, image: 'latte.jpg' },
-    { name: 'Bacon & Egg Sandwich', description: 'Bacon, Gouda, and Egg', price: 5.0, image: 'testing.jpg' },
-    { name: 'Turkey Bacon Sandwich', description: 'Bacon, Egg and Chedder', price: 5.0, image: 'testing.jpg' },
+    { name: 'Egg Sandwich', description: 'Chicken, Egg, and Mapple Butter', price: 2.5, image: '../assets/images/egg_sandwich2.jpg' },
+    { name: 'Sausage & Egg Wrap', description: 'Sausage, Chedder, and Egg', price: 3.0, image: '../assets/images/sausage_cheese.png' },
+    { name: 'Bacon & Egg Sandwich', description: 'Bacon, Gouda, and Egg', price: 5.0, image: '../assets/images/bacon_sandwich.jpg' },
+    { name: 'Turkey Bacon Sandwich', description: 'Bacon, Egg and Chedder', price: 5.0, image: '../assets/images/turkey_bacon.jpg' },
 
   ];
     
   snacks = [
-    { name: 'snack1', description: 'Strong and bold', price: 2.5, image: 'espresso.jpg' },
-    { name: 'snack2', description: 'Smooth and creamy', price: 3.0, image: 'latte.jpg' },
-    { name: 'Cheetos', description: 'Testing and testing', price: 5.0, image: 'testing.jpg' },
-    { name: 'Test3', description: 'Testing and testing', price: 5.0, image: 'testing.jpg' },
+    { name: 'Madeleines', description: 'Snacks', price: 2.5, image: '../assets/images/Madeleines.jpg' },
+    { name: 'Cookies', description: 'Sweets', price: 3.0, image: '../assets/images/Cookies.jpg' },
+    { name: 'String Cheese', description: 'Cheese', price: 5.0, image: '../assets/images/string_cheese.jpg' },
+    { name: 'Perfect Bar', description: 'Snack bar', price: 5.0, image: '../assets/images/PerfectBar.jpg' },
   ];
 }
